c914d82de57a74ada6d6b7971a5a8cd3094ecd27
CORP\nkosinathi.khumalo <Nkosinathi.Khumalo@irdeto.com>
2018-09-14 12:57:23 +0200

    Updates to program cat

--- quries.js
+++ quries.js
@@ -1,2 +1,28 @@
 //update docs
 db.logs.updateMany({"employee": "Alouw2"}, {$set: {"worktype": "NORMAL"}}, {multi:true})
+
+/*
+
+ firstname : String,
+    lastname : String,
+    consultingFirm : String,
+    company : String,
+    contractor : String,
+    contractorNumber : String,
+    clientContactName : String,
+    contractorContact : String,
+    username: String
+
+ */
+
+ {
+     "firstname":"",
+     "lastname":"",
+     "consultingFirm":"",
+     "company":"",
+     "contractor":"",
+     "contractorNumber":"",
+     "clientContactName":"",
+     "contractorContact":"",
+     "username":""
+ }
